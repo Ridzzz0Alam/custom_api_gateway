9e10f5b589012fcc03f7724cd055e5daa80151c2
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-07-01 12:29:22 +0200

    completed the circiut breaker config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -11,3 +11,19 @@ import (
 	"github.com/spf13/viper"
 )
 
+func init() {
+	serviceName := "api_gateway"
+
+	viper.SetDefault("server.port", "8080")
+	viper.SetDefault("admin.port", "8081")
+	viper.SetDefault("server.host", "localhost")
+	viper.SetDefault("server.metrics.prefix", serviceName)
+	viper.SetDefault("server.metrics.buckets", []float64{0.005, 0.01, 0.025, 0.05, 0.1})
+}
+
+type CircuitSettings struct {
+	Enabled			bool	`yaml:"enabled"`
+	Timeout			uint	`yaml:"timeout"`
+	Interval		uint	`yaml:"interval"`
+	FailureRatio	float64	`yaml:"failureRatio"`
+}
