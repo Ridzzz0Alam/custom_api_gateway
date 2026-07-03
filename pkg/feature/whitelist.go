a1718a81021e993d4465f9c86f479e61b01c3537
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-07-03 22:36:31 +0200

    completed the middleware

--- pkg/feature/whitelist.go
+++ pkg/feature/whitelist.go
@@ -4,8 +4,41 @@ type IPWhiteList struct {
 	Whitelist map[string]bool `json:whitelist`
 }
 
-func PopulateIPWhiteList(w *IPWhiteList, ipList []string){
+func PopulateIPWhiteList(w *IPWhiteList, ipList []string) {
 	if len(ipList) > 0 && ipList[0] == "ALL" {
-		
+		// Allowing all ip ranges
+		w.Whitelist["ALL"] = true
+	} else {
+		for _, ip := range ipList {
+			if ip == "ALL" {
+				continue
+			}
+			w.Whitelist[ip] = true
+		}
 	}
+}
+
+func NewIPWhiteList() *IPWhiteList {
+	w := IPWhiteList{
+		Whitelist: make(map[string]bool),
+	}
+	return &w
+}
+
+func (w *IPWhiteList) Allowed(ip string) bool {
+	if _, exists := w.Whitelist["ALL"]; exists {
+		return true
+	}
+	if _, found := w.Whitelist[ip]; !found {
+		return false
+	}
+	return true
+}
+
+func (w *IPWhiteList) GetWhiteList() map[string]bool {
+	return w.Whitelist
+}
+
+func (w *IPWhiteList) UpdateWhitelist(newList map[string]bool) {
+	w.Whitelist = newList
 }
