a009088c295dae3080a1e01cb1a903880fb16eb6
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-07-04 16:09:09 +0200

    completed the cache coding

--- pkg/feature/cache.go
+++ pkg/feature/cache.go
@@ -8,14 +8,46 @@ import (
 	"github.com/patrickmn/go-cache"
 )
 
-type CacheExpiration interface
+type CacheExpiration int
 
 const (
 	DefaultExpiration CacheExpiration = 0
-	NoExpiration CacheExpiration = -1 
+	NoExpiration      CacheExpiration = -1
 )
 
 type CacheHandler struct {
-	Enabled				bool `json:"enabled"`
-	ExpirationInterval	uint
+	Enabled            bool `json:"enabled"`
+	ExpirationInterval uint `json:"expirationInterval"`
+	CleanupInterval    uint `json:"cleanupInterval"`
+	cache              *cache.Cache
+}
+
+func NewCacheHdnler(conf *config.CacheSettings) *CacheHandler {
+	// If 0, set to default values
+
+	if conf.ExpirationInterval == 0 {
+		conf.ExpirationInterval = 5
+	}
+	if conf.Cleanupinterval == 0 {
+		conf.Cleanupinterval = 10
+	}
+	return &CacheHandler{
+		Enabled:            conf.Enabled,
+		ExpirationInterval: conf.ExpirationInterval,
+		CleanupInterval:    conf.Cleanupinterval,
+		cache: cache.New(time.Duration(conf.ExpirationInterval)*time.Second,
+			time.Duration(conf.Cleanupinterval)*time.Second),
+	}
+}
+
+func (c *CacheHandler) Get(key string) (any, bool) {
+	return c.cache.Get(key)
+}
+
+func (c *CacheHandler) Set(key string, value any, exp CacheExpiration) {
+	c.cache.Set(key, value, time.Duration(exp))
+}
+
+func (c *CacheHandler) IsEnabled() bool {
+	return c.Enabled
 }
