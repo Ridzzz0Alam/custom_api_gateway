98f9cd72881aed9b2e6c4e0b8f89e237a78aa78e
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-07-04 20:19:06 +0200

    fixed bugs in circuit breaker code

--- pkg/feature/circuit.go
+++ pkg/feature/circuit.go
@@ -3,32 +3,31 @@ package feature
 import (
 	"log/slog"
 
-	"github.com/ArmaanKaytal/pkg/config"
 	"github.com/Ridzzz0Alam/api_gateway/pkg/config"
 	"github.com/sony/gobreaker/v2"
 )
 
-type CirciutBreaker struct {
+type CircuitBreaker struct {
 	Settings config.CircuitSettings `json:"settings"`
 	breaker  *gobreaker.CircuitBreaker[[]byte]
 }
 
-func NewCirciutBreaker(svcName string, settings config.CircuitSettings) *CirciutBreaker {
-	return &CirciutBreaker{
+func NewCircuitBreaker(svcName string, settings config.CircuitSettings) *CircuitBreaker {
+	return &CircuitBreaker{
 		Settings: settings,
 		breaker:  gobreaker.NewCircuitBreaker[[]byte](settings.Into(svcName)),
 	}
 }
 
-func (cb *CirciutBreaker) Execute(service string, f func() ([]byte, error)) ([]byte, error) {
+func (cb *CircuitBreaker) Execute(service string, f func() ([]byte, error)) ([]byte, error) {
 	slog.Info("Forwarding request using circuit breaker", "service", service, "breaker", cb.breaker.Name)
 	return cb.breaker.Execute(f)
 }
 
-func (cb *CirciutBreaker) IsOpen() bool {
+func (cb *CircuitBreaker) IsOpen() bool {
 	return cb.breaker.State() == gobreaker.StateOpen
 }
 
-func (cb *CirciutBreaker) isEnabled() bool {
-	return cb.Settings.isEnabled
+func (cb *CircuitBreaker) IsEnabled() bool {
+	return cb.Settings.Enabled
 }
